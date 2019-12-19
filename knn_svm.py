6ebbb3d1cb7c02e87471cefa6f6a9c1389b2331a
Yongxin (Fiona) Xu <yxu1@haverford.edu>
2019-12-19 00:21:24 -0500

    add comments

--- knn_svm.py
+++ knn_svm.py
@@ -1,63 +1,74 @@
-# from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import GridSearchCV, StratifiedKFold
+"""
+KNN and SVM learning.
+Author: Yongxin (Fiona) Xu
+Date: 12/19/2019
+"""
+# from python libraries
 from sklearn.metrics import confusion_matrix
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
 
-import glob
 import matplotlib.pyplot as plt
 import numpy as np
 
 # from my own libraries
 import util
 
-'''
-def runTuneTest(learner, params, data):
-    
-    # divide the data into training/test 5 folds
-
-    for _ in range(5):
-        clf = GridSearchCV(learner, params, cv=3)
-        # fit the classifier using your training data
-        clf.fit(data['t_X'], data['t_y'])
-        # get paramenters for this fold
-        print(clf.best_params_)
-        # get the train-set accuracy
-        print("Training Score: ", clf.score(data['t_X'], data['t_y']))
-        print("Testing Score: ", clf.score(data['v_X'], data['v_y']))
-'''
-
 def train(learner, train_X, train_y, val_X, val_y):
     '''
-    Train a learner and return training and testing accuracy
+    Train a learner and return training and testing accuracy,
+    used for finding the best parameters
+    Args:
+        learner: an sklearn classifier
+        train_X(np.ndarray): training data's features
+        train_y(np.ndarray): training data's labels
+        val_X(np.ndarray): validation data's features
+        val_y(np.ndarray): validation data's labels
+    Returns:
+        (float): training score
+        (float): validation score
     '''
+    
     learner.fit(train_X, train_y)
     return learner.score(train_X, train_y), learner.score(val_X, val_y)
 
-def test(learner, train_X, train_y, val_X, val_y):
+
+def test(learner, train_X, train_y, val_X, val_y, filename):
+    """
+    Test a leaner, return training and testing accuracy and confusion matrix,
+    used for learning the performance of the model with the best parameters
+    Args:
+        learner: an sklearn classifier
+        train_X(np.ndarray): training data's features
+        train_y(np.ndarray): training data's labels
+        val_X(np.ndarray): validation data's features
+        val_y(np.ndarray): validation data's labels
+    """
+    
     learner.fit(train_X, train_y)
     y_pred = learner.predict(val_X)
+    print('Training accuracy: ', learner.score(train_X, train_y))
     print('Testing accuracy: ', learner.score(val_X, val_y))
-    print('Confusion matrix:\n', confusion_matrix(val_y, y_pred))
+    cm = confusion_matrix(val_y, y_pred)
+    print('Confusion matrix:\n', cm)
+    util.draw_heatmap(cm, filename)
+ 
             
 def main(plot):
     train_X, train_y, val_X, val_y = util.load_data('./data/semeion.data', 0.8)
     my_X, my_y = util.get_my_data('./data/testset.csv', './data/digits/')
-    
-    # data = dict(t_X=train_X, t_y=train_y, v_X=val_X, v_y=val_y)
-    
+
     # KNN
+    print('------KNN------')
     if plot:
         # find the best hyper-parameters
         # initialize lists for plotting graph
         uni_train_accur, uni_test_accur, dist_train_accur, dist_test_accur = ([] for _ in range(4))
         n_neighbors = np.arange(1,11)
         for neighbors in n_neighbors:
-            print(neighbors)
             for weight in ['uniform', 'distance']:
                 knn_clf = KNeighborsClassifier(neighbors, weights=weight)
-                train_accur, test_accur = train(knn_clf, train_X, train_y, val_X, val_y)
-                
+                train_accur, test_accur = train(knn_clf, train_X, train_y, val_X, val_y)                
                 if weight == 'uniform':
                     uni_train_accur.append(train_accur)
                     uni_test_accur.append(test_accur)
@@ -76,23 +87,25 @@ def main(plot):
         plt.show()
   
     # set weights as distance, and n_neighbors as 5
-    print('------KNN------')
     knn_clf = KNeighborsClassifier(5, weights='distance')
-    test(knn_clf, train_X, train_y, val_X, val_y)
-
-    
+    test(knn_clf, train_X, train_y, val_X, val_y, './img/knn_cm.png')
+ 
     # find the wrong predictions
     if plot:
         util.draw_wrong_img(val_X, val_y, y_pred, 'knn')
     
     my_y_pred = knn_clf.predict(my_X)
     print('Testing accuracy of our own data: ', knn_clf.score(my_X, my_y))
-    print('Confusion matrix of our own data:\n', confusion_matrix(my_y, my_y_pred))
+    cm = confusion_matrix(my_y, my_y_pred)
+    print('Confusion matrix of our own data:\n', cm)
+    util.draw_heatmap(cm, './img/knn_own.png')
 
     if plot:
-    util.draw_wrong_img(my_X, my_y, my_y_pred, 'knn_own')
+        util.draw_wrong_img(my_X, my_y, my_y_pred, 'knn_own')
+        
     
     # SVM
+    print('------SVM------')
     if plot:
         # find the best hyper-parameters
         # initialize lists for plotting graph
@@ -102,7 +115,6 @@ def main(plot):
             for C in [1., 10., 100.]:
                 svm_clf = SVC(C=C, gamma=g)
                 train_accur, test_accur = train(svm_clf, train_X, train_y, val_X, val_y)
-                
                 if C == 1.:
                     train1.append(train_accur)
                     test1.append(test_accur)
@@ -125,10 +137,9 @@ def main(plot):
         plt.savefig('./img/svm_score.png', bbox_inches="tight")
         plt.show()
   
-    # set C as 100, and gamma as 0.01
-    print('------SVM------')
+    # set C as 100, and gamma as 0.01 
     svm_clf = SVC(C=100, gamma=0.01)
-    test(svm_clf, train_X, train_y, val_X, val_y)
+    test(svm_clf, train_X, train_y, val_X, val_y, './img/svm_cm.png')
     
     # find the wrong predictions
     if plot:
@@ -136,10 +147,12 @@ def main(plot):
     
     my_y_pred = svm_clf.predict(my_X)
     print('Testing accuracy of our own data: ', svm_clf.score(my_X, my_y))
-    print('Confusion matrix of our own data:\n', confusion_matrix(my_y, my_y_pred))
-    
-    # if plot:
-    util.draw_wrong_img(my_X, my_y, my_y_pred, 'svm_own')
+    cm =  confusion_matrix(my_y, my_y_pred)
+    print('Confusion matrix of our own data:\n', cm)
+    util.draw_heatmap(cm, './img/svm_own.png')
+        
+    if plot:
+        util.draw_wrong_img(my_X, my_y, my_y_pred, 'svm_own')
         
 if __name__ == '__main__':
     main(False)
--- util.py
+++ util.py
@@ -1,36 +1,55 @@
 """
+Utils for KNN, SVM, and CNN learning.
+Author: Yongxin (Fiona) Xu
+Date: 12/18/2019
 """
 import csv
 import cv2
-import glob
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 import numpy as np 
 import seaborn as sns
 
 
-def load_data(path=str, threshold=float):
+def load_data(path, threshold):
+    '''
+    Load data from data file.
+    Args:
+        path(str): path to data file
+        threshod(float): ratio of training and validation dset,
+            e.g. 0.8 means 80% of data is training data and 20% is validation
+    Returns:
+        train_X(np.ndarray): training data's parameters
+        train_y(np.ndarray): training data's labels
+        val_X(np.ndarray): validation data's parameters
+        val_y(np.ndarray): validation data's labels
+    '''
     # load data
     data = np.loadtxt(path)
-
     # shuffle data
     np.random.seed(42)
-    np.random.shuffle(data)
-    
+    np.random.shuffle(data)   
     # extract X, y
     X = data[:, :-10]
     y = data[:, -10:]
-    y = np.argmax(y, axis=1) # convert one-hot encodings to integers
-
+    # convert one-hot encodings to integers
+    y = np.argmax(y, axis=1) 
     train_X, val_X = np.split(X, [int(threshold*len(X))])
-    train_y, val_y = np.split(y, [int(threshold*len(y))])
-    
+    train_y, val_y = np.split(y, [int(threshold*len(y))])   
     return train_X, train_y, val_X, val_y
+
 
 def draw_wrong_img(X, y, y_hat, filename):
     """
-    Regenerate the images when their labels are predicted wrong.
+    Generate the images of digits that were predicted wrong.
+    Args:
+        X(np.ndarray): data's parameters
+        y(np.ndarray): data's true labels
+        y_hat(np.ndarray): data's predicted labels
+        filename(str): name of generated images file, used for creating 
+            the path to save images (e.g. './img/(filename)/(filename).png')
     """
+    
     i = 1
     for input_, prediction, label in zip(X, y_hat, y):
         if prediction != label:
@@ -39,21 +58,50 @@ def draw_wrong_img(X, y, y_hat, filename):
             plt.title('{}: True label: {}. Prediction: {}'.format(filename.upper(), label, prediction))
             plt.savefig('{}.png'.format(name))
             i+=1
+  
             
 def process_image(img):
+    """
+    Use OpenCV to resize an image into 16x16 and turn it to numpy array with binary value.
+    Args:
+        img(str): path to an image
+    Returns:
+        (np.ndarray): numpy array representation of this image
+    """
+    
     img = cv2.imread(img, cv2.IMREAD_GRAYSCALE)
     img = cv2.resize(img, (16,16))
     img[img<=230]=1
     img[img>230]=0
     return img.reshape((256,))
 
-def transform_image(imgs):   
+
+def transform_image(imgs):
+    """
+    Transform a list of imgs into a 2-d numpy matrix, that is input's features.
+    Args:
+        imgs(list): a list str type paths to images
+    Returns:
+        (np.dnarry): input's features
+    """
+    
     X = []
     for img in imgs:
         X.append(process_image(img))
     return np.array(X)
 
+
 def read_csv(path, img_path):
+    """
+    Read CSV file
+    Args:
+        path(str): path to the CSV file
+        img_path(str): path to the image files
+    Returns:
+        imgs(list): list of paths to iamge files
+        label(np.dnarray): input's label
+    """
+    
     with open(path, encoding='utf-8-sig') as file:
         readCSV = csv.reader(file, delimiter=',')
         imgs = []
@@ -63,37 +111,41 @@ def read_csv(path, img_path):
             label.append(float(row[1]))
         label = np.array(label)
         return imgs, label
+
     
 def get_my_data(path, img_path):
+    """
+    Get self-generated data, i.e. image data
+    Args:
+        path(str): path to the CSV file
+        img_path(str): path to the image files
+    Returns:
+        features(np.dnarray): input's features
+        label(np.dnarray): input's label
+    """
+    
     imgs, label = read_csv(path, img_path)
     features = transform_image(imgs)
     return features, label
 
 
 def draw_heatmap(array, filename):
+    """
+    Draw a heatmap based on the confusion matrix for visualization
+    Args:
+        array(np.dnarray): confusion matrix
+        filename(str): path to save the plot
+    """
+    
     ax = sns.heatmap(array, linewidth=0.2, annot=True, cmap="YlGnBu")
     plt.savefig(filename)
     plt.show()
+ 
     
 def main():
     train_X, train_y, val_X, val_y = load_data('./data/semeion.data', 0.8)
     
-    # i = 900
-    # x = train_X[i]
-    # print(x.shape)
-    print(val_X.shape)
-   
-    # plt.imshow(x, cmap="gray_r")
-    # plt.savefig('test.png')
-    # plt.show()
     
-    imgs, label = read_csv('./data/testset.csv', './data/digits/')
-    imgs = transform_image(imgs)  
-    print(imgs)
-    
-      
-    
-
 if __name__ == '__main__':
     main()
 
